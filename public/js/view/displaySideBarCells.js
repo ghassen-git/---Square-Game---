8b76b49e0496cde0adf686718ef2054aedbb5a6f
ghassen <ghassenguesmi@icloud.com>
2024-12-03 14:23:06 +0100

    fix/style_score1

--- public/js/view/displaySideBarCells.js
+++ public/js/view/displaySideBarCells.js
@@ -4,5 +4,5 @@ const sideBar = document.querySelector(".pieces-container");
 
 export const displaySideBar = function (obj) {
   console.log(Object.keys(obj).length);
-  sideBar.innerHTML = generateSideBarCellsBox(Object.keys(obj).length - 2);
+  sideBar.innerHTML = generateSideBarCellsBox(Object.keys(obj).length);
 };
